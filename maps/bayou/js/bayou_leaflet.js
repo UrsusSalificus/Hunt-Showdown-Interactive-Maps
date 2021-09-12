ccab9d668ce509b267557bbd20dd32d8737d79e1
Titouan Bougouin-Laessle <titouan.bougouin@gmail.com>
2021-09-12 17:51:53 +0200

    bayou first 3 markers ready
    + icons

--- maps/bayou/js/bayou_leaflet.js
+++ maps/bayou/js/bayou_leaflet.js
@@ -1,37 +1,81 @@
-///// GENERAL FOR LEAFLET /////
+// LOCATIONS //
+var screenshotsLocation = "images/screenshots/"
 var mapImageLocation = 'images/bayou_map.png';
+var markersFile = "bayou_markers.csv"
+
+// LEAFLET MAP //
 var map = L.map('bayouMap', {crs: L.CRS.Simple});
 var bounds = [[0,0], [100,100]];
 var image = L.imageOverlay(mapImageLocation, bounds).addTo(map);
 
-// markers
-// ref: http://stackoverflow.com/a/1293163/2343
-// This will parse a delimited string into an array of
-// arrays. The default delimiter is the comma, but this
-// can be overriden in the second argument.
-function startSeriesRetrieval(file) {
-  // sent a GET request to retrieve the CSV file contents
-  $.get(file, function( CSVdata) {
-     // CSVdata is populated with the file contents
-     // ready to be converted into an Array
-      data = $.csv.toArray(CSVdata);
-      return data
+// MARKERS //
+// Icons //
+// This will assign the right icon to the marker, providing the icon type
+function assignIcon(iconType) {
+  var outsideIcon = L.icon({
+      iconUrl: '../../icons/outside_poster.svg',
+      iconSize:     [30, 36], // size of the icon
+      iconAnchor:   [15, 18], // point of the icon which will correspond to marker's location -> need center = half of iconSize
+      popupAnchor:  [-30, -36] // point from which the popup should open relative to the iconAnchor
   });
-};
 
-console.log(startSeriesRetrieval("bayou_markers.csv"))
+  var insideIcon = L.icon({
+      iconUrl: '../../icons/inside_poster.svg',
+      iconSize:     [40, 50], // size of the icon
+      iconAnchor:   [20, 25], // point of the icon which will correspond to marker's location -> need center = half of iconSize
+      popupAnchor:  [-40, -50] // point from which the popup should open relative to the iconAnchor
+  });
 
-var sol = L.latLng([ 5, 10]);
-L.marker(sol).addTo(map)
-
-/*
-function addingMarkersToMap(allMarkers, map) {
-  for (var i = 0; i < allMarkers.length; i++) {
-    L.marker(allMarkers[i]).addTo(map);
-  }
-  return
+  var undergroundIcon = L.icon({
+      iconUrl: '../../icons/underground_poster.svg',
+      iconSize:     [40, 50], // size of the icon
+      iconAnchor:   [20, 25], // point of the icon which will correspond to marker's location -> need center = half of iconSize
+      popupAnchor:  [-40, -50] // point from which the popup should open relative to the iconAnchor
+  });
+  if (iconType === 'outside') {
+    return outsideIcon
+  } else if (iconType === 'inside') {
+    return insideIcon
+  } else return undergroundIcon
 }
-*/
+
+
+// Importing marker file
+var CSVFile = $.get(file)
+var CSVData = CSVFile["responseText"].split(/\r?\n/)0]
+// Test data
+//var CSVData = ['type,leftCoord,rightCoord,icon,screenshot', 'poster,83.5,20,inside,alain_1.jpg', 'poster,81,15.2,inside,alain_2.jpg','poster,82.5,17.3,outside,alain_3.jpg', '']
+
+// We will parse every line of the markers file, skipping first (headers)
+// and last entry (empty)
+function addingMarkersToMap(CSVData, screenshotsLocation, map) {
+  for (var i = 1; i < (CSVData.length -1); i++) {
+    var markerData = CSVData[i].split(',')
+
+    // Variables needed to add a marker
+    var markerType = markerData[0]
+    var leftCoord = markerData[1]
+    var rightCoord = markerData[2]
+    var iconType = markerData[3]
+    var screenshot = markerData[4]
+
+    var icon = assignIcon(iconType)
+    // Adding the marker
+    var coords = L.latLng([ leftCoord, rightCoord])
+    L.marker(coords, {icon: icon})
+      .bindPopup("<div class='popupContainer'><img src=" + "'" + screenshotsLocation + screenshot + "'" + " class=popupImage></div>")
+      .on('mouseover', function (e) {
+         var popup = e.target.getPopup();
+         popup.setLatLng(e.latlng).openOn(map);
+       })
+       .on('mouseout', function(){
+         this.closePopup();
+       })
+      .addTo(map);
+  }
+}
+
+addingMarkersToMap(CSVData, screenshotsLocation, map)
 
 // Function to input in console when on the page to display
 function addGridToPinpointMarker (){
@@ -46,6 +90,9 @@ function addGridToPinpointMarker (){
        var popup = e.target.getPopup();
        popup.setLatLng(e.latlng).openOn(map);
       })
+      .on('mouseout', function(){
+        this.closePopup();
+      })
      .addTo(map);
   }
   // Vertical lines
@@ -56,11 +103,12 @@ function addGridToPinpointMarker (){
        var popup = e.target.getPopup();
        popup.setLatLng(e.latlng).openOn(map);
       })
+      .on('mouseout', function(){
+        this.closePopup();
+      })
      .addTo(map);
   }
 }
 
-
-
-
+// Launching map ?
 map.fitBounds(bounds);
